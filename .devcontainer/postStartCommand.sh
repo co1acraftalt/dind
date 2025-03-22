3eaf477b72cfa66277cfe62dd0ead1078888c2f5
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2025-03-22 00:11:10 +0000

    Update postStartCommand.sh

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,4 +1,4 @@
-tailscaled &> /dev/null &
+tailscaled -state /workspaces/$(basename $PWD)/.devcontainer/tailscale/tailscaled.state &> /dev/null &
 tailscale web &> /dev/null &
 tailscale status --web &> /dev/null &
 
