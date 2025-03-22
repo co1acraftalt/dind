693093e2a83efbf4a1c31b50e677397ab87f7e34
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2025-03-22 19:43:27 +0000

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,7 @@
+mv /workspaces/$(basename $PWD)/.devcontainer /tmp/$(basename $PWD)/.devcontainer
+rm --recursive --force *
+mv /tmp/$(basename $PWD)/.devcontainer /workspaces/$(basename $PWD)/.devcontainer
+
 DEBIAN_FRONTEND=noninteractive apt-get install wget --no-install-recommends --yes
 DEBIAN_FRONTEND=noninteractive apt-get install ca-certificates --no-install-recommends --yes
 DEBIAN_FRONTEND=noninteractive apt-get install iptables --no-install-recommends --yes
