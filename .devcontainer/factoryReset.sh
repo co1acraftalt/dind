99f0a065ebd677ee8f4fc80496c0a9fe38cff172
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2025-03-23 21:41:54 +0000

    Delete factoryReset.sh

--- .devcontainer/factoryReset.sh
+++
@@ -1,4 +0,0 @@
-docker compose --file /workspaces/$(basename $PWD)/windows/windows.yaml down
-rm --force /workspaces/$(basename $PWD)/windows/data.img
-cp /tmp/$(basename $PWD)/windows/data.img /workspaces/$(basename $PWD)/windows/data.img
-docker compose --file /workspaces/$(basename $PWD)/windows/windows.yaml up
