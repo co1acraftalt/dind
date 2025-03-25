774f028fc8899045178fe3862d23dd59a672bc0a
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2025-03-25 18:14:59 +0000

    Update postStartCommand.sh

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,7 +1,7 @@
 export REPOSITORY=dind
 
+dockerd --seccomp-profile unconfined --experimental &> /dev/null &
+
 tailscaled -statedir /workspaces/$REPOSITORY/.devcontainer/tailscale &> /dev/null &
 tailscale web &> /dev/null &
 tailscale status --web &> /dev/null &
-
-dockerd --seccomp-profile unconfined --experimental &> /dev/null &
