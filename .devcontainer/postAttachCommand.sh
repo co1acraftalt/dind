c6f671aa25170f7ab7aa0b63df2a6207df355f67
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2025-04-04 21:09:06 +0100

    Update postAttachCommand.sh

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -3,3 +3,4 @@ tailscale up --hostname codespaces --advertise-exit-node --qr
 clear
 echo "IPv4: $(tailscale ip --4)"
 echo "IPv6: $(tailscale ip --6)"
+echo "DNS: $(tailscale whois $(tailscale ip --4) | grep -oP '[a-z0-9-]+\.[a-z0-9-]+\.ts\.net')"
