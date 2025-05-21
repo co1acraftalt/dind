5f2876885b5cfd8df210cf68f236a8100e4e7447
co1acraftalt <al6143@outlook.com>
2025-05-21 12:59:09 -0400

    Update onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,20 +1,20 @@
-export REPOSITORY=github
+export REPOSITORY=dind
 
 rm --recursive --force /tmp/$REPOSITORY
 mkdir --parents /tmp/$REPOSITORY/windows
-mv --force /workspaces/$REPOSITORY/.devcontainer /tmp/$REPOSITORY/.devcontainer
+mv --force /workspace/$REPOSITORY/.devcontainer /tmp/$REPOSITORY/.devcontainer
 
-rm --recursive --force /workspaces/$REPOSITORY
-mkdir --parents /workspaces/$REPOSITORY/windows
-mv --force /tmp/$REPOSITORY/.devcontainer /workspaces/$REPOSITORY/.devcontainer
+rm --recursive --force /workspace/$REPOSITORY
+mkdir --parents /workspace/$REPOSITORY/windows
+mv --force /tmp/$REPOSITORY/.devcontainer /workspace/$REPOSITORY/.devcontainer
 
-cp /workspaces/$REPOSITORY/.devcontainer/helpers/start /usr/local/bin/start
-cp /workspaces/$REPOSITORY/.devcontainer/helpers/stop /usr/local/bin/stop
-cp /workspaces/$REPOSITORY/.devcontainer/helpers/restart /usr/local/bin/restart
-cp /workspaces/$REPOSITORY/.devcontainer/helpers/reset /usr/local/bin/reset
-cp /workspaces/$REPOSITORY/.devcontainer/helpers/rebuild /usr/local/bin/rebuild
-cp /workspaces/$REPOSITORY/.devcontainer/helpers/remove /usr/local/bin/remove
-cp /workspaces/$REPOSITORY/.devcontainer/helpers/kill /usr/local/bin/kill
+cp /workspace/$REPOSITORY/.devcontainer/helpers/start /usr/local/bin/start
+cp /workspace/$REPOSITORY/.devcontainer/helpers/stop /usr/local/bin/stop
+cp /workspace/$REPOSITORY/.devcontainer/helpers/restart /usr/local/bin/restart
+cp /workspace/$REPOSITORY/.devcontainer/helpers/reset /usr/local/bin/reset
+cp /workspace/$REPOSITORY/.devcontainer/helpers/rebuild /usr/local/bin/rebuild
+cp /workspace/$REPOSITORY/.devcontainer/helpers/remove /usr/local/bin/remove
+cp /workspace/$REPOSITORY/.devcontainer/helpers/kill /usr/local/bin/kill
 chmod +x /usr/local/bin/start
 chmod +x /usr/local/bin/stop
 chmod +x /usr/local/bin/restart
@@ -32,11 +32,11 @@ wget https://github.com/ItzLevvie/artifacts/releases/download/27774-2/data.7z.00
 rm --force /tmp/$REPOSITORY/windows/data.7z.00*
 qemu-img convert -p -O raw -o preallocation=off /tmp/$REPOSITORY/windows/data.vhdx /tmp/$REPOSITORY/windows/data.img
 rm --force /tmp/$REPOSITORY/windows/data.vhdx
-cp /tmp/$REPOSITORY/windows/data.img /workspaces/$REPOSITORY/windows/data.img
+cp /tmp/$REPOSITORY/windows/data.img /workspace/$REPOSITORY/windows/data.img
 
 {
     echo "data.img"
-} > /workspaces/$REPOSITORY/windows/windows.boot
+} > /workspace/$REPOSITORY/windows/windows.boot
 
 {
     echo "services:"
@@ -46,7 +46,7 @@ cp /tmp/$REPOSITORY/windows/data.img /workspaces/$REPOSITORY/windows/data.img
     echo "    environment:"
     echo "      CPU_CORES: $(nproc --all)"
     echo "      RAM_SIZE: $(free --gibi | grep 'Mem:' | awk '{print $7}')G"
-    echo "      DISK_SIZE: $(df --human-readable --block-size G /workspaces | grep '/workspaces' | awk '{print $4}')"
+    echo "      DISK_SIZE: $(df --human-readable --block-size G /workspace | grep '/workspace' | awk '{print $4}')"
     echo "      DISK2_SIZE: $(df --human-readable --block-size G /tmp | grep '/tmp' | awk '{print $4}')"
     echo "      TPM: Y"
     echo "      RAM_CHECK: N"
@@ -69,12 +69,12 @@ cp /tmp/$REPOSITORY/windows/data.img /workspaces/$REPOSITORY/windows/data.img
     echo "    security_opt:"
     echo "      - seccomp=unconfined"
     echo "    volumes:"
-    echo "      - /workspaces/$REPOSITORY/windows:/storage"
+    echo "      - /workspace/$REPOSITORY/windows:/storage"
     echo "      - /tmp/$REPOSITORY/windows:/storage2"
-    echo "      - /workspaces/$REPOSITORY:/data"
+    echo "      - /workspace/$REPOSITORY:/data"
     echo "    privileged: true"
     echo "    restart: on-failure"
-} > /workspaces/$REPOSITORY/windows/windows.yaml
+} > /workspace/$REPOSITORY/windows/windows.yaml
 
 echo ""
 
